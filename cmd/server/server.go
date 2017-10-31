62d2545cae249025da32e5f1a0f9d758004fb2d0
hmli <hmli>
2017-10-31 09:31:59 +0800

    finish

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,39 +3,81 @@ package main
 import (
 	"net"
 	"fmt"
+	"time"
 	"io"
+	"sync"
+	"flag"
 )
-
-
+// Server side: -sport: dst, -cport:  给client side开的端口
+// Client side: -sport: 上面的cport, -cport: 给用户开的端口
 func main() {
-	clientPort := "6999"
-	serverPort := "7000"
+	flagServerPort := flag.String("sport", "8000", "Server port number")
+	flagClientPort := flag.String("cport", "6999", "Client port number")
+	flag.Parse()
+	clientPort := *flagClientPort
+	localServerPort := *flagServerPort
 
-	connClient, err := net.Dial("tcp", ":"+clientPort)
+
+	listener, err := net.Listen("tcp", ":"+clientPort)
 	if err != nil {
 		panic(err)
 	}
-
-	listener, err := net.Listen("tcp", ":"+serverPort)
-	if err != nil {
-		panic(err)
-	}
-	defer connClient.Close()
+	//defer listenServer.Close()
 	defer listener.Close()
+	//go test(clientPort)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("err:", err)
 			continue
 		}
-		io.Copy(conn, connClient)
-		io.Copy(connClient, conn)
-		conn.Close()
-
+		connServer, err := net.Dial("tcp", ":"+localServerPort)
+		if err != nil {
+			panic(err)
+		}
+		wg := sync.WaitGroup{}
+		wg.Add(2)
+		go connCopy(connServer, conn, &wg)
+		go connCopy(conn, connServer, &wg)
+		wg.Wait()
 	}
+}
 
+func connCopy(dst net.Conn, src net.Conn, wg *sync.WaitGroup) {
+	written, err := io.Copy(dst, src)
+	fmt.Println(written, err)
+	wg.Done()
+	dst.Close()
+}
 
+func test(port string){
+
+	ticker := time.NewTicker(5 * time.Second)
+	for {
+		select {
+		case <- ticker.C:
+			conn, err := net.Dial("tcp", ":"+port)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println(conn.Write([]byte("dddd")))
+
+			//n, err := conn.Read([]byte("12bbb"))
+			//fmt.Println(n, err)
+		}
+	}
 
 }
 
+
+//func pipe(connRev net.Conn, connSed net.Conn) {
+//	var buf []byte
+//	_, err := connRev.Write(buf)
+//	if err != nil {
+//		fmt.Println(err)
+//		return
+//	}
+//	connSed.Read(buf)
+//}
 
