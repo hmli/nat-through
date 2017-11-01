3a6db0e9988ed3fc114803baebd65d4e164c151a
hmli <lihm@welomo.com>
2017-11-02 00:44:37 +0800

    clear useless code

--- server.go
+++ server.go
@@ -17,14 +17,11 @@ func main() {
 	clientPort := *flagClientPort
 	localServerPort := *flagServerPort
 
-
 	listener, err := net.Listen("tcp", ":"+clientPort)
 	if err != nil {
 		panic(err)
 	}
-	//defer listenServer.Close()
 	defer listener.Close()
-	//go test(clientPort)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -40,6 +37,7 @@ func main() {
 		go connCopy(connServer, conn, &wg)
 		go connCopy(conn, connServer, &wg)
 		wg.Wait()
+		conn.Close()
 	}
 }
 
@@ -49,35 +47,3 @@ func connCopy(dst net.Conn, src net.Conn, wg *sync.WaitGroup) {
 	wg.Done()
 	dst.Close()
 }
-
-func test(port string){
-
-	ticker := time.NewTicker(5 * time.Second)
-	for {
-		select {
-		case <- ticker.C:
-			conn, err := net.Dial("tcp", ":"+port)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println(conn.Write([]byte("dddd")))
-
-			//n, err := conn.Read([]byte("12bbb"))
-			//fmt.Println(n, err)
-		}
-	}
-
-}
-
-
-//func pipe(connRev net.Conn, connSed net.Conn) {
-//	var buf []byte
-//	_, err := connRev.Write(buf)
-//	if err != nil {
-//		fmt.Println(err)
-//		return
-//	}
-//	connSed.Read(buf)
-//}
-
